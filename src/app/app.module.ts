d2dec15895e360841d08d46b40f53bf72800a810
newmankyle <newmankyleSWL@gmail.com>
2020-03-18 11:52:53 -0400

    adds sidenav. Refactors CSS so spacing axiom respects two-column format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,12 @@ import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { TodoItemComponent } from "./components/todo-item/todo-item.component";
 import { TodoListComponent } from "./components/todo-list/todo-list.component";
 import { TodoListModule } from "./components/todo-list/todo-list.module";
 import { TodoAdderComponent } from "./components/todo-adder/todo-adder.component";
-import { CustomCardHeaderComponent } from './components/custom-card-header/custom-card-header.component';
+import { CustomCardHeaderComponent } from "./components/custom-card-header/custom-card-header.component";
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { CustomCardHeaderComponent } from './components/custom-card-header/custo
     MatIconModule,
     MatTooltipModule,
     MatButtonModule,
+    MatSidenavModule,
     TodoListModule
   ],
   providers: [],
